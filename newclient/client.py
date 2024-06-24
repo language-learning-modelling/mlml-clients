884150d986d80b31700360dce3fbd1df80ce1d32
bernardo <bernardo.stearns@gmail.com>
2024-06-24 18:29:33 +0000

    setting up roberta model

--- newclient/client.py
+++ newclient/client.py
@@ -3,7 +3,7 @@ import sys
 
 training_fp = sys.argv[1]
 
-downloader = Downloader("./run_configs/download_bert_base_uncased.json")
+downloader = Downloader("./run_configs/download_xlm_roberta_base.json")
 downloader.downloadLocally()
 
 trainer = Trainer(training_fp)
