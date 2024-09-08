10aa549e951df4b517d08d022802e592cbd950a7
bernardo <bernardo.stearns@gmail.com>
2024-09-08 12:29:37 +0100

    added trainerConfig

--- newclient/train_client.py
+++ newclient/train_client.py
@@ -1,13 +1,32 @@
 from mlml_hugginface import Downloader
 from mlml_hugginface.train import Trainer
 import sys
+import os
 
-training_config_fp = sys.argv[1]
 
-if len(sys.argv) > 2:
-    download_config_fp = sys.argv[2]
-    downloader = Downloader(download_config_fp)
-    downloader.downloadLocally()
+@dataclass
+class TrainerConfig:
+    MODEL_CHECKPOINT: str
+    DATASET_NAME: str
+    HF_CHECKPOINT: str = None
+    LORA: bool = False
+    MLM_PROBABILITY: float = 0.15
+    BATCH_SIZE: int = 16
 
+    def __post_init__(self):
+        for field_key in self.__dataclass_fields__.keys():
+            if self.__getattribute__(field_key) is None:
+             raise ValueError(f'missing {field_key} config property')
+
+
+training_config_jsonStr_or_fp = "".join(sys.argv[1:])
+
+if os.path.exists(training_config_jsonStr_or_fp):
+    with open(training_config_jsonStr_or_fp) as inpf:
+        config = json.load(inpf)
+else:
+        config = json.loads(training_config_jsonStr_or_fp)
+    
+trainerConfig(**{k.upper(): v for k, v in config.items()})
 trainer = Trainer(training_config_fp)
 trainer.train()
