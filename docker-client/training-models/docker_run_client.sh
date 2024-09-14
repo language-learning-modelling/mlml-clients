dbb04ff830077413f1d435e341274364ce8466c4
bernardo <bernardo.stearns@gmail.com>
2024-09-14 18:20:54 +0100

    changed config to reflect base_model vs training_checkpoint

--- docker-client/training-models/docker_run_client.sh
+++ docker-client/training-models/docker_run_client.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# alias d=./docker_run_client.sh;d `jo -p model_checkpoint="bert-base-uncased" dataset_name="efcamdat" split="TRAIN" training_strategy="FULL+LLM-TOKENIZE"`  
-# THE MAIN OBJECTIVE of this client is getting configs for setting up the infrastructure and launch docker
+# alias d=./docker_run_client.sh;d `jo -p base_model_name="bert-base-uncased" run_hash="2024-09-14-15-01" training_checkpoint="checkpoint-150" dataset_name="efcamdat" split="TRAIN" training_strategy="FULL+LLM-TOKENIZE"`
 # Ideally it will choose between running docker or simply running as a bahs script
 
 
--- newclient/train_client.py
+++ newclient/train_client.py
@@ -16,23 +16,23 @@ class TrainingStrategy(Enum):
 # Automatically generate reverse mapping from the enum values
 TRAINING_STRATEGY_MAP = {strategy.value: strategy for strategy in TrainingStrategy}
 
-@dataclass
-class TrainerConfig:
-    MODEL_CHECKPOINT: str
-    DATASET_NAME: str
-    DATASET_FOLDER: str = "datasets"
-    SPLIT: str = None
-    HF_CHECKPOINT: bool = False
-    LORA: bool = False
-    MLM_PROBABILITY: float = 0.15
-    BATCH_SIZE: int = 4
-
-    # Allow training_strategy as a string input, which will be converted to enum
-    TRAINING_STRATEGY: str = field(default="FULL+LLM-TOKENIZE")
-
-    def __post_init__(self):
-        required_fields = ["MODEL_CHECKPOINT", "DATASET_NAME"]
-        for field_key in self.__dataclass_fields__.keys():
+@dataclass                                                                                             
+class TrainerConfig:                                                                                   
+    BASE_MODEL_NAME: str = None                                                                        
+    RUN_HASH: str = None                                                                               
+    TRAINING_CHECKPOINT: str = None                                                                    
+    DATASET_NAME: str = None                                                                           
+    DATASET_FOLDER: str = "datasets"                                                                   
+    SPLIT: str = None                                                                                  
+    HF_CHECKPOINT: bool = False                                                                        
+    LORA: bool = False                                                                                 
+    MLM_PROBABILITY: float = 0.15                                                                      
+    BATCH_SIZE: int = 4                                                                                
+    # Allow training_strategy as a string input, which will be converted to enum                       
+    TRAINING_STRATEGY: str = field(default="FULL+LLM-TOKENIZE")                                        
+                                                                                                       
+    def __post_init__(self):                                                                           
+        required_fields = ["BASE_MODEL_NAME", "DATASET_NAME"]   for field_key in self.__dataclass_fields__.keys():
             if field_key in required_fields and self.__getattribute__(field_key) is None:
                 raise ValueError(f'missing {field_key} config property')
 
